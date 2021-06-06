48bb87a9305c43bf51787b038edfa8c3a277a937
chris <chris.herb@gmail.com>
2021-06-05 23:32:28 -0400

    initial

---
+++ src/test/java/chris/doberman/reactive/ReactiveExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package chris.doberman.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
